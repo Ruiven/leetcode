2c99a352dd89490eef4fa8d6d3c4d1edf836d18e
Ruiwen.Zhao <zhaorw93@gmail.com>
2015-08-11 21:28:22 +0800

    new problems added

--- leetcode-Populating Next Right Pointers in Each Node/src/main/Solution.java
+++ leetcode-Populating Next Right Pointers in Each Node/src/main/Solution.java
@@ -1,5 +1,6 @@
 package main;
 
+
 class TreeLinkNode {
 	int val;
 	TreeLinkNode left, right, next;
@@ -7,7 +8,35 @@ class TreeLinkNode {
 }
 
 public class Solution {
-	public void connect(TreeLinkNode root) {
+	public static void connect(TreeLinkNode root) {
+		 if(root == null){  
+	            return;  
+	        }  
+	          
+	       
+	        if(root.left != null){  
+	            root.left.next = root.right;  
+	        }  
+	          
+	        if(root.right!=null && root.next!=null){  
+	            root.right.next = root.next.left;  
+	        }  
+	          
+	        connect(root.left);  
+	        connect(root.right);  
         
     }
+	
+	public static void main(String[] args) {
+		TreeLinkNode head = new TreeLinkNode(1);
+		head.left = new TreeLinkNode(2);
+		head.right = new TreeLinkNode(3);
+		head.left.left = new TreeLinkNode(4);
+		head.left.right = new TreeLinkNode(5);
+		head.right.left = new TreeLinkNode(6);
+		connect(head);
+		System.out.print("");
+		
+		
+	}
 }
